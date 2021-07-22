2ee0c2ef350f6378c6fb5c6a9d8d434452f756bf
Alexander Leech <alexander.leech@capita.co.uk>
2021-07-22 16:07:59 +0100

    navbar setup with background colour and useStyles, still needs linking and burger menu adding

--- src/components/organisms/navbar.js
+++ src/components/organisms/navbar.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 
+const useStyles = makeStyles((theme) => ({
+    bgGrey: {
+        backgroundColor: '#D3D3D3',
+    },
+  }));
+
 function NavBar() {
+    const classes = useStyles();
+
     return (
-        <div>navbarrrrr</div>
+        <AppBar position="static" className={classes.bgGrey}>
+            <Toolbar>
+                <Typography variant="h6">
+                    Luksy Designs Customs
+                </Typography>
+            </Toolbar>
+        </AppBar>
     );
 }
 
