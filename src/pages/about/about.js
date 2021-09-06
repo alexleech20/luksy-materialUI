6890a8f27e75c8e76f2e566de4f63617ee43c423
Alex Leech <alexleech546@gmail.com>
2021-09-06 02:04:27 +0100

    added react helmet, started about page (WIP to switch over machines)

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { makeStyles, Grid, Typography, Card, CardActionArea, CardContent, CardMedia } from '@material-ui/core';
+import { Helmet } from 'react-helmet';
+import image from './headshot.png';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 345,
+        maxHeight: 345,
+    },
+}));
+
+const TITLE = "About Us";
 
 function About() {
+
+    const classes = useStyles();
+
     return (
         <div>
-            <h1>WELCOME PEOPLE to about page</h1>
+            <Helmet>
+                <title>
+                    { TITLE }
+                </title>
+            </Helmet>
+            <Grid container>
+                <Grid xs={12} md={4}>
+                    <Card className={classes.root}>
+                        <CardActionArea>
+                            <CardMedia 
+                                className={classes.media}
+                                image="./headshot.png"
+                                src={image}
+                                title="Lukman's headshot"
+                            />
+                            {/* <img className={classes.media} src={image} alt="" /> */}
+                            <CardContent>
+                                <Typography variant="h5" component="h2">
+                                    Lukman
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+            </Grid>
         </div>
     );
 }
